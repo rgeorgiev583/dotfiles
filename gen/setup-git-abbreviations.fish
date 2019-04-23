9ddf988c3f71b65a03a72e07bbf68711e5c27e61
Radoslav Georgiev <radoslav.georgiev@securax.org>
2019-04-23 15:34:48 +0300

    dotfile generator: add script for generating abbreviations for fish from the oh-my-zsh git aliases

---
+++ gen/setup-git-abbreviations.fish
@@ -0,0 +1,4 @@
+#!/usr/bin/fish
+
+mkdir -p $HOME/.config/fish/conf.d
+wget -O- -q https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/plugins/git/git.plugin.zsh | grep '^alias' | sed -E 's/^alias (.+)=\'(.+)\'$/abbr -a -g \1 \2/' | sed -E 's/^alias (.+)="(.+)"$/abbr -a -g \1 \2/' > $HOME/.config/fish/conf.d/git-abbreviations.fish
