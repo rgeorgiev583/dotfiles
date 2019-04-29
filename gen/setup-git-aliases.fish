1152de7e26937890e70f8f1be43117957b9a9ab4
Radoslav Georgiev <rgeorgiev583@gmail.com>
2019-04-29 14:01:31 +0300

    dotfile generator: fish: refactor: reduce the script for generating Git aliases to a single pipeline

--- gen/setup-git-aliases.fish
+++ gen/setup-git-aliases.fish
@@ -1,6 +1,1 @@
-wget -O /tmp/git.plugin.zsh https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/plugins/git/git.plugin.zsh
-grep '^alias' /tmp/git.plugin.zsh | sed 's/^alias/alias -s/' > /tmp/git.plugin.fish
-
-source /tmp/git.plugin.fish
-
-rm /tmp/git.plugin.zsh /tmp/git.plugin.fish
+wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/plugins/git/git.plugin.zsh | grep '^alias' | sed 's/^alias/alias -s/' | source
