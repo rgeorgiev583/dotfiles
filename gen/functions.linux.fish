50ab43749c67487b97d822bf1a9635b9bc53e10c
Radoslav Georgiev <rgeorgiev583@gmail.com>
2019-05-02 21:48:24 +0300

    dotfile generator: fish: fix a bug where the argument list for the `grep` function would be appended to the end of the definition instead of the end of the first command in the pipeline

--- gen/functions.linux.fish
+++ gen/functions.linux.fish
@@ -3,7 +3,10 @@ function diff
 end
 funcsave diff
 
-alias -s grep 'grep --color | less --tabs=4 -RFX'
+function grep
+	command grep --color $argv | less --tabs=4 -RFX
+end
+funcsave grep
 
 alias -s makej 'make -j'(math (nproc) + 1)
 alias -s cachemake "make CC='ccache gcc' CXX='ccache g++'"
