ce24243a273962a694fe9b796bc46d047fb8da17
Radoslav Georgiev <radoslav.georgiev@securax.org>
2019-06-06 17:16:50 +0300

    dotfile generator: fish: make the generation of aliases/abbreviations for Git optional

--- gen/setup.fish
+++ gen/setup.fish
@@ -1,8 +1,10 @@
 fish setup-functions.fish
 fish setup-abbreviations.fish
-if test $argv[1] = alias
-	fish setup-git-aliases.fish
-else
-	fish setup-git-abbreviations.fish
+if test $argv[1] = generate-git
+	if test $argv[2] = alias
+		fish setup-git-aliases.fish
+	else
+		fish setup-git-abbreviations.fish
+	end
 end
 fish setup-fisher.fish
