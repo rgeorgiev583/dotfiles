b85d2e4025dd8784fe2d86fa0a4c7d66602ba744
Radoslav Georgiev <radoslav.georgiev@securax.org>
2019-06-06 17:35:27 +0300

    dotfile generator: fish: implement copying of git aliases in the setup script

--- gen/setup.fish
+++ gen/setup.fish
@@ -6,5 +6,11 @@ if test $argv[1] = generate-git
 	else
 		fish setup-git-abbreviations.fish
 	end
+else if test $argv[1] = copy-git
+	if test $argv[2] = aliases; and test -f ../.config/fish/conf.d/git-aliases.fish
+		cp ../.config/fish/conf.d/git-aliases.fish ~/.config/fish/conf.d/
+	else if test -f ../.config/fish/conf.d/git-abbreviations.fish
+		cp ../.config/fish/conf.d/git-abbreviations.fish ~/.config/fish/conf.d/
+	end
 end
 fish setup-fisher.fish
