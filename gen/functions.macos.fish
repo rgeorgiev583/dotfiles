9bd4cf71d30f621729c0e9c3bc023c4acb4319ff
Radoslav Georgiev <radoslav.georgiev@securax.org>
2019-06-06 17:05:31 +0300

    dotfile generator: fish: convert the `grep` alias to a function so that the arguments could be passed to `ggrep` itself

--- gen/functions.macos.fish
+++ gen/functions.macos.fish
@@ -3,7 +3,10 @@ function diff
 end
 funcsave diff
 
-alias -s grep 'ggrep --color | less --tabs=4 -RFX'
+function grep
+	ggrep --color $argv | less --tabs=4 -RFX
+end
+funcsave grep
 
 alias -s makej 'make -j'(math (sysctl -n hw.ncpu) + 1)
 alias -s cachemake "make CC='ccache gcc' CXX='ccache g++'"
