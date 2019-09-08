c1a535687a4bbc9d4f15ecd60ace2c15736f81c0
Radoslav Georgiev <rgeorgiev583@gmail.com>
2019-09-08 21:33:33 +0300

    dotfile generator: fish: rename the `generate-git aliases` and `copy-git aliases` subcommand lines respectively to `generate git-aliases` and `copy git-aliases`

--- gen/setup.fish
+++ gen/setup.fish
@@ -2,14 +2,14 @@ mkdir -p $HOME/.config/fish/conf.d
 
 fish setup-functions.fish
 fish setup-abbreviations.fish
-if test (count $argv) -ge 1; and test $argv[1] = generate-git
-	if test (count $argv) -ge 2; and test $argv[2] = aliases
+if test (count $argv) -ge 1; and test $argv[1] = generate
+	if test (count $argv) -ge 2; and test $argv[2] = git-aliases
 		fish setup-git-aliases.fish
 	else
 		fish setup-git-abbreviations.fish
 	end
-else if test (count $argv) -ge 1; and test $argv[1] = copy-git
-	if test (count $argv) -ge 2; and test $argv[2] = aliases; and test -f ../.config/fish/conf.d/git-aliases.fish
+else if test (count $argv) -ge 1; and test $argv[1] = copy
+	if test (count $argv) -ge 2; and test $argv[2] = git-aliases; and test -f ../.config/fish/conf.d/git-aliases.fish
 		cp ../.config/fish/conf.d/git-aliases.fish $HOME/.config/fish/conf.d/
 	else if test -f ../.config/fish/conf.d/git-abbreviations.fish
 		cp ../.config/fish/conf.d/git-abbreviations.fish $HOME/.config/fish/conf.d/
