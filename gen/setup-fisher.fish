eb716bbb0f501b8067f098763e846b4af73fa042
Radoslav Georgiev <rgeorgiev583@gmail.com>
2019-09-09 01:19:51 +0300

    dotfile generator: fish: implement installation of `fisher` through `pacman` on Arch-based distros

--- gen/setup-fisher.fish
+++ gen/setup-fisher.fish
@@ -1,5 +1,9 @@
 #!/usr/bin/env fish
 
 if not functions -q fisher
-    curl https://git.io/fisher --create-dirs -sLo $HOME/.config/fish/functions/fisher.fish
+	if grep -E '(ID|ID_LIKE)=arch' /etc/os-release
+		sudo pacman -S --needed fisher
+	else 
+	    curl https://git.io/fisher --create-dirs -sLo $HOME/.config/fish/functions/fisher.fish
+	end
 end
