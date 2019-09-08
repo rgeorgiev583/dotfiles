2bc5f3c42afef0d29eab76056eeb2e651b0e785c
Radoslav Georgiev <rgeorgiev583@gmail.com>
2019-09-09 00:37:44 +0300

    dotfile generator: fish: make the `copy` action the default one for the script which sets up Git helpers

--- gen/setup-git.fish
+++ gen/setup-git.fish
@@ -1,15 +1,15 @@
-if test (count $argv) -ge 1
-	if test $argv[1] = generate
-		if test (count $argv) -ge 2; and test $argv[2] = git-aliases
-			fish generate-git-aliases.fish
-		else
-			fish generate-git-abbreviations.fish
-		end
-	else if test $argv[1] = copy
-		if test (count $argv) -ge 2; and test $argv[2] = git-aliases; and test -f ../.config/fish/conf.d/git-aliases.fish
-			cp ../.config/fish/conf.d/git-aliases.fish $HOME/.config/fish/conf.d/
-		else if test -f ../.config/fish/conf.d/git-abbreviations.fish
-			cp ../.config/fish/conf.d/git-abbreviations.fish $HOME/.config/fish/conf.d/
-		end
+if test (count $argv) -ge 1; and test $argv[1] = generate
+	if test (count $argv) -ge 2; and test $argv[2] = git-aliases
+		fish generate-git-aliases.fish
+	else
+		fish generate-git-abbreviations.fish
+	end
+end
+
+if test (count $argv) -eq 0; or test $argv[1] = copy
+	if test (count $argv) -ge 2; and test $argv[2] = git-aliases; and test -f ../.config/fish/conf.d/git-aliases.fish
+		cp ../.config/fish/conf.d/git-aliases.fish $HOME/.config/fish/conf.d/
+	else if test -f ../.config/fish/conf.d/git-abbreviations.fish
+		cp ../.config/fish/conf.d/git-abbreviations.fish $HOME/.config/fish/conf.d/
 	end
 end
--- gen/setup.fish
+++ gen/setup.fish
@@ -2,6 +2,6 @@ mkdir -p $HOME/.config/fish/conf.d
 
 fish setup-functions.fish
 fish setup-abbreviations.fish
-fish setup-git.fish copy
+fish setup-git.fish
 fish setup-fisher.fish
 fish setup-fzf.fish
