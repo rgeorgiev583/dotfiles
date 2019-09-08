eee1c0960a9f6958af1099e016e2bd35f98e950a
Radoslav Georgiev <rgeorgiev583@gmail.com>
2019-09-09 02:27:55 +0300

    dotfile generator: fish: fix some style issues

--- gen/setup-fisher.fish
+++ gen/setup-fisher.fish
@@ -4,6 +4,6 @@ if not functions -q fisher
 	if grep -E '(ID|ID_LIKE)=arch' /etc/os-release
 		sudo pacman -S --needed fisher
 	else 
-	    curl https://git.io/fisher --create-dirs -sLo $HOME/.config/fish/functions/fisher.fish
+		curl https://git.io/fisher --create-dirs -sLo $HOME/.config/fish/functions/fisher.fish
 	end
 end
