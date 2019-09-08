465fa72276b08d743386f150026554c0ada5dce7
Radoslav Georgiev <rgeorgiev583@gmail.com>
2019-09-08 22:21:47 +0300

    dotfile generator: fish: fix an invocation of `wget` so that it would output to the standard output

--- gen/setup-git-aliases.fish
+++ gen/setup-git-aliases.fish
@@ -1,1 +1,1 @@
-wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/plugins/git/git.plugin.zsh | grep '^alias' | sed 's/^alias/alias -s/' | source
+wget -O- -q https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/plugins/git/git.plugin.zsh | grep '^alias' | sed 's/^alias/alias -s/' | source
