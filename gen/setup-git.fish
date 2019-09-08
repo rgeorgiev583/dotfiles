eda9bf0cba1e18baede0059ae8fe008fcb430555
Radoslav Georgiev <rgeorgiev583@gmail.com>
2019-09-08 21:58:10 +0300

    dotfile generator: fish: abstract out the generating of Git helpers into a separate script

---
+++ gen/setup-git.fish
@@ -0,0 +1,15 @@
+if test (count $argv) -ge 1
+	if test $argv[1] = generate
+		if test (count $argv) -ge 2; and test $argv[2] = git-aliases
+			fish setup-git-aliases.fish
+		else
+			fish setup-git-abbreviations.fish
+		end
+	else if test $argv[1] = copy
+		if test (count $argv) -ge 2; and test $argv[2] = git-aliases; and test -f ../.config/fish/conf.d/git-aliases.fish
+			cp ../.config/fish/conf.d/git-aliases.fish $HOME/.config/fish/conf.d/
+		else if test -f ../.config/fish/conf.d/git-abbreviations.fish
+			cp ../.config/fish/conf.d/git-abbreviations.fish $HOME/.config/fish/conf.d/
+		end
+	end
+end
--- gen/setup.fish
+++ gen/setup.fish
@@ -2,19 +2,5 @@ mkdir -p $HOME/.config/fish/conf.d
 
 fish setup-functions.fish
 fish setup-abbreviations.fish
-if test (count $argv) -ge 1
-	if test $argv[1] = generate
-		if test (count $argv) -ge 2; and test $argv[2] = git-aliases
-			fish setup-git-aliases.fish
-		else
-			fish setup-git-abbreviations.fish
-		end
-	else if test $argv[1] = copy
-		if test (count $argv) -ge 2; and test $argv[2] = git-aliases; and test -f ../.config/fish/conf.d/git-aliases.fish
-			cp ../.config/fish/conf.d/git-aliases.fish $HOME/.config/fish/conf.d/
-		else if test -f ../.config/fish/conf.d/git-abbreviations.fish
-			cp ../.config/fish/conf.d/git-abbreviations.fish $HOME/.config/fish/conf.d/
-		end
-	end
-end
+fish setup-git.fish copy
 fish setup-fisher.fish
