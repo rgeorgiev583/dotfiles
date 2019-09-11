6c93a03232144c8c05754e0bd6df7c22c061aa29
Radoslav Georgiev <radoslav.georgiev@securax.org>
2019-09-11 17:20:33 +0300

    dotfile generator: fish: fix a check which would yield a false positive on macOS because its implementation of `grep` exits with a zero status code when the file does not exist

--- gen/setup-fisher.fish
+++ gen/setup-fisher.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 
 if not functions -q fisher
-	if grep -E '(ID|ID_LIKE)=arch' /etc/os-release > /dev/null
+	if test -f /etc/os-release; and grep -E '(ID|ID_LIKE)=arch' /etc/os-release > /dev/null
 		pamac build fisher
 	else
 		curl https://git.io/fisher --create-dirs -sLo $HOME/.config/fish/functions/fisher.fish
