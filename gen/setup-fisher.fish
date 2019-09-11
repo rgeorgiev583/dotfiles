7184db4773748f70bce8217af0ef4f8f30143014
Radoslav Georgiev <radoslav.georgiev@securax.org>
2019-09-11 17:20:00 +0300

    dotfile generator: fish: fix some whitespace issues

--- gen/setup-fisher.fish
+++ gen/setup-fisher.fish
@@ -3,7 +3,7 @@
 if not functions -q fisher
 	if grep -E '(ID|ID_LIKE)=arch' /etc/os-release > /dev/null
 		pamac build fisher
-	else 
+	else
 		curl https://git.io/fisher --create-dirs -sLo $HOME/.config/fish/functions/fisher.fish
 	end
 end
