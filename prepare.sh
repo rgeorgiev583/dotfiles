7e18a74091fbe5f5a42cbc4314e9d460b3ecc53e
Radoslav Georgiev <rgeorgiev583@gmail.com>
2019-12-09 18:49:41 +0200

    prepare script: install dependencies only if needed

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if grep -E '(ID|ID_LIKE)=arch' /etc/os-release > /dev/null; then
-	sudo pacman -S neovim diff-so-fancy
+	sudo pacman -S --needed neovim diff-so-fancy
 else
 	echo 'error: OS not supported' >&2
 fi
