02e5cdcdf74763bc4a7da75fc816a38eb9fbaf2c
Radoslav Georgiev <rgeorgiev583@gmail.com>
2019-12-09 18:24:06 +0200

    dotfile generator: fish: switch to `yay` as the AUR helper

--- gen/setup-fisher.fish
+++ gen/setup-fisher.fish
@@ -2,7 +2,7 @@
 
 if not functions -q fisher
 	if test -f /etc/os-release; and grep -E '(ID|ID_LIKE)=arch' /etc/os-release > /dev/null
-		pamac build fisher
+		yay -S --needed --answerclean None --answerdiff None --answeredit None --answerupgrade None fisher
 	else
 		curl https://git.io/fisher --create-dirs -sLo $HOME/.config/fish/functions/fisher.fish
 	end
