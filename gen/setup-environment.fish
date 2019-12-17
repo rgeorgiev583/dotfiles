0c481eb38cd13800be231ccca24bb8b8673d5fe7
Radoslav Georgiev <rgeorgiev583@gmail.com>
2019-12-17 18:47:56 +0200

    dotfile generator: fish: make the script which setups the environment variable configuration immediately apply said configuration

--- gen/setup-environment.fish
+++ gen/setup-environment.fish
@@ -1,14 +1,18 @@
 #!/usr/bin/env fish
 
 cp environment.fish $HOME/.config/fish/conf.d/_environment.fish
+source $HOME/.config/fish/conf.d/_environment.fish
 
 switch (uname)
 	case Linux
 		cp environment.linux.fish $HOME/.config/fish/conf.d/_environment.linux.fish
+		source $HOME/.config/fish/conf.d/_environment.linux.fish
 	case Darwin
 		cp environment.macos.fish $HOME/.config/fish/conf.d/_environment.macos.fish
+		source $HOME/.config/fish/conf.d/_environment.macos.fish
 	case 'MSYS*'
 		cp environment.msys.fish $HOME/.config/fish/conf.d/_environment.msys.fish
+		source $HOME/.config/fish/conf.d/_environment.msys.fish
 	case '*'
 		echo 'error: OS not supported' >&2
 end
