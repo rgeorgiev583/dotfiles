3af8d8228b87412fb912de7ebfc0767d4f22dfdc
Radoslav Georgiev <rgeorgiev583@gmail.com>
2019-12-17 18:45:23 +0200

    dotfile generator: fish: make the definition of the MAKEFLAGS envvar platform-specific

---
+++ gen/environment.macos.fish
@@ -0,0 +1,2 @@
+# run `make` with as many jobs in parallel as there as processors
+set --export MAKEFLAGS -j(sysctl -n hw.ncpu)
--- gen/setup-environment.fish
+++ gen/setup-environment.fish
@@ -1,3 +1,14 @@
 #!/usr/bin/env fish
 
 cp environment.fish $HOME/.config/fish/conf.d/_environment.fish
+
+switch (uname)
+	case Linux
+		cp environment.linux.fish $HOME/.config/fish/conf.d/_environment.linux.fish
+	case Darwin
+		cp environment.macos.fish $HOME/.config/fish/conf.d/_environment.macos.fish
+	case 'MSYS*'
+		cp environment.msys.fish $HOME/.config/fish/conf.d/_environment.msys.fish
+	case '*'
+		echo 'error: OS not supported' >&2
+end
