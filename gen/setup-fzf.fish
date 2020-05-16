aefb058b5267f19a643996afc896a7ae8bd51292
Radoslav Georgiev <rgeorgiev583@gmail.com>
2020-05-16 21:30:44 +0300

    dotfile generator: fish: use default keybindings for fzf

--- gen/setup-fzf.fish
+++ gen/setup-fzf.fish
@@ -1,5 +1,5 @@
 #!/usr/bin/env fish
 
 if type fzf > /dev/null 2> /dev/null
-	fisher add jethrokuan/fzf
+	cp fish_user_key_bindings.fish $HOME/.config/fish/functions/fish_user_key_bindings.fish
 end
