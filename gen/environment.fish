b81c47acc054b6fd938c8233f5ab64bee87dc377
Radoslav Georgiev <rgeorgiev583@gmail.com>
2020-05-17 04:24:49 +0300

    dotfile generator: fish: fix a bug where user paths would be prepended multiple times to PATH

--- gen/environment.fish
+++ gen/environment.fish
@@ -20,4 +20,4 @@ set --export MAKEFLAGS -j(nproc)
 
 ## Linux-specific definitions
 
-set PATH $HOME/.gem/ruby/2.7.0/bin $HOME/.gem/ruby/2.6.0/bin $HOME/.node_modules_global/bin $HOME/.cargo/bin $HOME/go/bin $HOME/.local/bin $PATH
+set --universal PATH $HOME/.gem/ruby/2.7.0/bin $HOME/.gem/ruby/2.6.0/bin $HOME/.node_modules_global/bin $HOME/.cargo/bin $HOME/go/bin $HOME/.local/bin $PATH
