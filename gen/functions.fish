c161ba32325012acf2400302d680432d23a1ea9f
Radoslav Georgiev <rgeorgiev583@gmail.com>
2020-10-21 14:14:39 +0300

    dotfile generator: fish: add various wrapper commands for ripgrep and fd

--- gen/functions.fish
+++ gen/functions.fish
@@ -5,10 +5,29 @@ function rgl
 end
 funcsave rgl
 
+function rga
+	rg --hidden --no-ignore $argv
+end
+funcsave rga
+
+function rgal
+	rga --line-number --color=always $argv | less --tabs=4 -RFX
+end
+funcsave rgal
+
 function fdl
 	fd --color=always $argv | less --tabs=4 -RFX
 end
 funcsave fdl
+
+function fda
+	fd --hidden --no-ignore $argv
+end
+funcsave fda
+
+function fdal
+	fda --color=always $argv | less --tabs=4 -RFX
+end
 
 function nvim
 	command nvim -s (echo ':silent call xolox#session#auto_load()' | psub) $argv
