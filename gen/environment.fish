cc1fc0606fe975f012e91c499347201c4315e3bf
Radoslav Georgiev <rgeorgiev583@gmail.com>
2021-03-22 00:48:13 +0200

    gen: fish: remove some unused user paths

--- gen/environment.fish
+++ gen/environment.fish
@@ -20,4 +20,4 @@ set --export MAKEFLAGS -j(nproc)
 
 ## Linux-specific definitions
 
-set fish_user_paths $HOME/.gem/ruby/2.7.0/bin $HOME/.gem/ruby/2.6.0/bin $HOME/.node_modules_global/bin $HOME/.cargo/bin $HOME/go/bin $HOME/.local/bin
+set fish_user_paths $HOME/.gem/ruby/2.7.0/bin $HOME/.node_modules_global/bin $HOME/.cargo/bin $HOME/go/bin $HOME/.local/bin
