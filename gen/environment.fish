4b520326096c6f0689fad879914d1a22eac9fa26
Radoslav Georgiev <rgeorgiev583@gmail.com>
2021-03-22 00:50:46 +0200

    gen: fish: use a lesskey file instead of LESS_TERMCAP_* envvars
    Their values used to break the output of programs like env(1).

--- gen/environment.fish
+++ gen/environment.fish
@@ -2,15 +2,6 @@
 set --export EDITOR nvim
 set --export VISUAL nvim
 
-# colorize man pages
-set --export LESS_TERMCAP_mb \e\[01\;32m
-set --export LESS_TERMCAP_md \e\[01\;32m
-set --export LESS_TERMCAP_me \e\[0m
-set --export LESS_TERMCAP_se \e\[0m
-set --export LESS_TERMCAP_so \e\[01\;47\;34m
-set --export LESS_TERMCAP_ue \e\[0m
-set --export LESS_TERMCAP_us \e\[01\;36m
-
 # set Oh My Neovim path and list of extensions to load
 set --export OH_MY_NEOVIM $HOME/.oh-my-neovim
 set --export OH_MY_NEOVIM_EXTENSIONS 'default code_style git gpg icons json mouse neomake search session spell true_color undo xml yaml'
