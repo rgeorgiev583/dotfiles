d7173436c0033c7abb1bba86592658c6a34a935c
Radoslav Georgiev <rgeorgiev583@gmail.com>
2021-10-27 20:36:46 +0300

    prepare.fish: totally remove support of non-Arch systems
    In addition, simplify the script and remove superfluous Yay arguments.

--- prepare.fish
+++ prepare.fish
@@ -1,16 +1,8 @@
 #!/bin/fish
 
-if egrep -q '(ID|ID_LIKE)=arch' /etc/os-release
-	yay --sync --needed neovim fzf diff-so-fancy fd ripgrep
-	yay --sync --needed --answerclean None --answerdiff None --answeredit None --answerupgrade None git-delta
-else
+if not egrep -q '(ID|ID_LIKE)=arch' /etc/os-release
 	echo 'error: OS not supported' >&2
+	exit 1
 end
 
-if not functions --query fisher
-	if test -f /etc/os-release; and egrep '(ID|ID_LIKE)=arch' /etc/os-release > /dev/null
-		yay --sync --needed --answerclean None --answerdiff None --answeredit None --answerupgrade None fisher
-	else
-		curl https://git.io/fisher --create-dirs --silent --location --output $HOME/.config/fish/functions/fisher.fish
-	end
-end
+yay --sync --needed neovim fzf diff-so-fancy git-delta fd ripgrep fisher
