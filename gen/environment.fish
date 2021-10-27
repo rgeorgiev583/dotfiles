d1ffa4ab884c820bb67c0720fa0492c215c298f0
Radoslav Georgiev <rgeorgiev583@gmail.com>
2021-10-27 21:57:59 +0300

    gen/environment.fish: declare the environment variables in the universal scope
    This way we'd avoid having a separate configuration script for them:
    they'd simply be stored in the fish_variables file.  That's why the
    script becomes a configuration generator (i.e. it has to be sourced
    instead of being copied to the fish config dir), and is moved
    appropriately.

---
+++ gen/environment.fish
@@ -0,0 +1,20 @@
+# set Neovim as the default editor
+set --universal --export EDITOR nvim
+set --universal --export VISUAL nvim
+
+# set Oh My Neovim path and list of extensions to load
+set --universal --export OH_MY_NEOVIM $HOME/.oh-my-neovim
+set --universal --export OH_MY_NEOVIM_EXTENSIONS 'default code_style git gpg icons json mouse neomake search session spell true_color undo xml yaml'
+
+# run `make` with as many jobs in parallel as there as processors
+set --universal --export MAKEFLAGS -j(nproc)
+
+# make `fzf` search through hidden files as well
+set --universal --export FZF_DEFAULT_COMMAND 'fd -t f -t l 2> /dev/null'
+
+# make `ripgrep` read its config file
+set --universal --export RIPGREP_CONFIG_PATH $HOME/.config/ripgreprc
+
+## Linux-specific definitions
+
+set fish_user_paths $HOME/.gem/ruby/2.7.0/bin $HOME/.node_modules_global/bin $HOME/.cargo/bin $HOME/go/bin $HOME/.local/bin
