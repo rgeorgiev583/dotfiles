f064bc607373e565fc7bbdc3c3bcc0589348d932
Radoslav Georgiev <rgeorgiev583@gmail.com>
2021-10-27 20:26:35 +0300

    merge setup-fisher.fish functionality into the prepare script

--- prepare.fish
+++ prepare.fish
@@ -1,4 +1,11 @@
-#!/usr/bin/env fish
+#!/bin/fish
+
+if egrep -q '(ID|ID_LIKE)=arch' /etc/os-release
+	yay --sync --needed neovim fzf diff-so-fancy fd ripgrep
+	yay --sync --needed --answerclean None --answerdiff None --answeredit None --answerupgrade None git-delta
+else
+	echo 'error: OS not supported' >&2
+end
 
 if not functions --query fisher
 	if test -f /etc/os-release; and egrep '(ID|ID_LIKE)=arch' /etc/os-release > /dev/null
