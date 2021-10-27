274e1223d9b6351cf6aa03fb26433311594b74fa
Radoslav Georgiev <rgeorgiev583@gmail.com>
2021-10-27 17:12:09 +0300

    gen/functions.fish: remove the nvim-session functionality

--- gen/functions.fish
+++ gen/functions.fish
@@ -27,18 +27,6 @@ function fdal
 	fda --color=always $argv | less --tabs=4 -RFX
 end
 
-function nvim-session
-	if test (count $argv) -eq 0
-		set session $PWD
-	else
-		set session $argv[1]
-	end
-	#nvim -s (begin; echo :silent CloseSession; echo ":silent! OpenSession $session"; echo ":silent SaveSession $session"; end | psub) $argv[2..-1]
-	#command nvim -s (begin; echo ":silent MakeSession $session"; echo ":silent OpenSession $session"; end | psub) $argv[2..-1]
-	command nvim -s (begin; echo ":silent! OpenSession $session"; echo ":silent SaveSession $session"; end | psub) $argv[2..-1]
-end
-funcsave nvim-session
-
 function diff
 	command diff --unified --color=always $argv | delta
 end
