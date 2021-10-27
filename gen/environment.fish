189f20485df409cab133419ac5e8c7c3cf0c6f33
Radoslav Georgiev <rgeorgiev583@gmail.com>
2021-10-27 17:08:53 +0300

    gen/environment.fish: improve comments

--- gen/environment.fish
+++ gen/environment.fish
@@ -9,10 +9,12 @@ set --export OH_MY_NEOVIM_EXTENSIONS 'default code_style git gpg icons json mous
 # run `make` with as many jobs in parallel as there as processors
 set --export MAKEFLAGS -j(nproc)
 
-# make `fzf` search using fd(1)
+# make `fzf` search through hidden files as well
 set --export FZF_DEFAULT_COMMAND 'fd -t f -t l 2> /dev/null'
 
 # make `ripgrep` read its config file
 set --export RIPGREP_CONFIG_PATH $HOME/.config/ripgreprc
+
+## Linux-specific definitions
 
 set fish_user_paths $HOME/.gem/ruby/2.7.0/bin $HOME/.node_modules_global/bin $HOME/.cargo/bin $HOME/go/bin $HOME/.local/bin
