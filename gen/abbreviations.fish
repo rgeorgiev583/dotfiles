b4ec93eb10372fc38851cb5e3adb73dda8790540
Radoslav Georgiev <rgeorgiev583@gmail.com>
2021-10-27 17:05:36 +0300

    gen/abbreviations.fish: add abbrs for docker and docker-compose

--- gen/abbreviations.fish
+++ gen/abbreviations.fish
@@ -2,5 +2,10 @@ if set -q EDITOR
 	abbr --add e $EDITOR
 end
 
+abbr --add d docker
+abbr --add dc docker-compose
+
+## Linux-specific definitions
+
 abbr --add sc 'systemctl'
 abbr --add jc 'journalctl'
