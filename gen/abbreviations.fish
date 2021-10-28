526eb24a8ee06bbd32b999584930a35cd831ed5c
Radoslav Georgiev <rgeorgiev583@gmail.com>
2021-10-29 01:39:22 +0300

    gen/abbreviations.fish: move `free` to the list of Linux-specific definitions

--- gen/abbreviations.fish
+++ gen/abbreviations.fish
@@ -3,7 +3,6 @@ if set -q EDITOR
 end
 
 abbr --add df df -h
-abbr --add free free -h
 abbr --add less less --tabs=4 -RFX
 abbr --add rga rg --hidden --no-ignore
 abbr --add fda fd --hidden --no-ignore
@@ -12,5 +11,6 @@ abbr --add dc docker-compose
 
 ## Linux-specific definitions
 
+abbr --add free free -h
 abbr --add sc 'systemctl'
 abbr --add jc 'journalctl'
