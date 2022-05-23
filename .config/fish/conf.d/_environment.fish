30ec318717452185871cce0ed399142aac4c3819
Radoslav Georgiev <rgeorgiev583@gmail.com>
2022-05-23 11:08:03 +0300

    nvim: replace the `oh-my-neovim` config with LunarVim (`lvim`)

--- .config/fish/conf.d/_environment.fish
+++ .config/fish/conf.d/_environment.fish
@@ -5,10 +5,6 @@ set --global --export VISUAL nvim
 # set default diff tool
 set --global --export DIFFPROG 'nvim -d'
 
-# set Oh My Neovim path and list of extensions to load
-set --global --export OH_MY_NEOVIM $HOME/.oh-my-neovim
-set --global --export OH_MY_NEOVIM_EXTENSIONS 'default code_style git gpg icons json mouse search session spell true_color undo xml yaml'
-
 # run `make` with as many jobs in parallel as there as processors
 set --global --export MAKEFLAGS -j(nproc)
 
--- .config/nvim/plugin/switch_header_source.vim
+++
@@ -1,8 +0,0 @@
-if exists('g:switch_header_source#loaded')
-    finish
-endif
-let g:switch_header_source#loaded = 1
-
-let g:switch_header_source#file_ext_mapping = {'h': ['c', 'cc', 'cpp', 'cxx', 'm', 'mm'], 'hh': ['cc', 'mm'], 'hpp': ['cpp'], 'hxx': ['cxx'], 'c': ['h'], 'cc': ['h', 'hh'], 'cpp': ['h', 'hpp'], 'cxx': ['h', 'hxx'], 'm': ['h'], 'mm': ['h', 'hh']}
-
-command SwitchHeaderSource :call switch_header_source#SwitchHeaderSource()
