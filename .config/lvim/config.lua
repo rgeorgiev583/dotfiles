3c06c9fa7402711d3cbbc462619b8907f9f3cc7b
Radoslav Georgiev <rgeorgiev583@gmail.com>
2022-08-17 10:09:19 +0000

    lvim: add `yaml-companion.nvim` extension

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -278,6 +278,17 @@ lvim.plugins = {
     config = function()
       require("dapui").setup()
     end
+  },
+  {
+    "someone-stole-my-name/yaml-companion.nvim",
+    requires = {
+      { "neovim/nvim-lspconfig" },
+      { "nvim-lua/plenary.nvim" },
+      { "nvim-telescope/telescope.nvim" },
+    },
+    config = function()
+      require("telescope").load_extension("yaml_schema")
+    end,
   }
 }
 
