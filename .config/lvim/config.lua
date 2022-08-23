35ee5c53879ce710f0c6a711cbdf0a6f95800b06
Radoslav Georgiev <rgeorgiev583@gmail.com>
2022-08-23 11:58:04 +0000

    lvim: improve `nvim-spectre` window keybindings

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -335,7 +335,75 @@ lvim.plugins = {
     "windwp/nvim-spectre",
     event = "BufRead",
     config = function()
-      require("spectre").setup()
+      require("spectre").setup({
+        mapping = {
+          -- toggle_line = {
+          --   map = "dd",
+          --   cmd = "<cmd>lua require('spectre').toggle_line()<CR>",
+          --   desc = "toggle current item"
+          -- },
+          -- enter_file = {
+          --   map = "<CR>",
+          --   cmd = "<cmd>lua require('spectre.actions').select_entry()<CR>",
+          --   desc = "goto current file"
+          -- },
+          send_to_qf = {
+            map = "<leader>Sq",
+            cmd = "<cmd>lua require('spectre.actions').send_to_qf()<CR>",
+            desc = "send all item to quickfix"
+          },
+          replace_cmd = {
+            map = "<leader>Sc",
+            cmd = "<cmd>lua require('spectre.actions').replace_cmd()<CR>",
+            desc = "input replace vim command"
+          },
+          show_option_menu = {
+            map = "<leader>So",
+            cmd = "<cmd>lua require('spectre').show_options()<CR>",
+            desc = "show option"
+          },
+          run_current_replace = {
+            map = "<leader>Sr",
+            cmd = "<cmd>lua require('spectre.actions').run_current_replace()<CR>",
+            desc = "replace current line"
+          },
+          run_replace = {
+            map = "<leader>SR",
+            cmd = "<cmd>lua require('spectre.actions').run_replace()<CR>",
+            desc = "replace all"
+          },
+          change_view_mode = {
+            map = "<leader>Sv",
+            cmd = "<cmd>lua require('spectre').change_view()<CR>",
+            desc = "change result view mode"
+          },
+          change_replace_sed = {
+            map = "<leader>Ses",
+            cmd = "<cmd>lua require('spectre').change_engine_replace('sed')<CR>",
+            desc = "use sed to replace"
+          },
+          change_replace_oxi = {
+            map = "<leader>Seo",
+            cmd = "<cmd>lua require('spectre').change_engine_replace('oxi')<CR>",
+            desc = "use oxi to replace"
+          },
+          toggle_live_update = {
+            map = "<leader>Sl",
+            cmd = "<cmd>lua require('spectre').toggle_live_update()<CR>",
+            desc = "update change when vim write file."
+          },
+          toggle_ignore_case = {
+            map = "<leader>Si",
+            cmd = "<cmd>lua require('spectre').change_options('ignore-case')<CR>",
+            desc = "toggle ignore case"
+          },
+          toggle_ignore_hidden = {
+            map = "<leader>Sh",
+            cmd = "<cmd>lua require('spectre').change_options('hidden')<CR>",
+            desc = "toggle search hidden"
+          },
+        },
+      })
     end
   },
   { "jessarcher/vim-heritage" },
