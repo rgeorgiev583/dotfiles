222592255d06d2fb12891ebe1e93e7199f570325
Radoslav Georgiev <rgeorgiev583@gmail.com>
2022-10-19 21:26:59 +0300

    install: copy the dotfiles instead of creating symlinks to them
    This simplifies the installation logic.

--- install.sh
+++ install.sh
@@ -1,24 +1,12 @@
 #!/bin/bash
 
-function overwrite_with_symlink_in_homedir {
-    local entry=$1
-    rm -rf -- "$HOME/${entry:?}"
-    ln -sf "$(realpath "$entry")" "$HOME/$entry"
-}
-
-mkdir -p "$HOME/.config"
 for entry in .*; do
     case $entry in
     . | .. | .git | .gitignore)
         continue
         ;;
-    .config)
-        for config_entry in .config/*; do
-            overwrite_with_symlink_in_homedir "$config_entry"
-        done
-        ;;
     *)
-        overwrite_with_symlink_in_homedir "$entry"
+        rsync -a "$entry" "$HOME"
         ;;
     esac
 done
