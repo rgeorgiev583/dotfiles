287234a0e758a264f8965949ee871f27c1d402df
Radoslav Georgiev <rgeorgiev583@gmail.com>
2022-10-20 10:57:52 +0300

    prepare: convert to Bash

--- prepare.sh
+++ prepare.sh
@@ -1,8 +1,8 @@
-#!/bin/fish
+#!/bin/bash
 
-if not egrep -q '(ID|ID_LIKE)=arch' /etc/os-release
+if ! grep -Eq '(ID|ID_LIKE)=arch' /etc/os-release; then
 	echo 'error: OS not supported' >&2
 	exit 1
-end
+fi
 
 yay --sync --needed neovim fzf git-delta fd ripgrep fisher nerd-fonts-hack
