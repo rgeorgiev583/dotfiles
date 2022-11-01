17b872b6fc4f6e1d2f497127941f60a87bfd5b45
Radoslav Georgiev <rgeorgiev583@gmail.com>
2022-11-01 14:36:25 +0200

    generator scripts: convert the generator scripts to Bash

--- generate-git-aliases.sh
+++ generate-git-aliases.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env fish
+#!/bin/bash
 
 wget --output-document=- --quiet https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/plugins/git/git.plugin.zsh | grep '^alias' > ../.config/fish/conf.d/git-aliases.fish
