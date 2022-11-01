c588fab60fca208dd9d580cd7362daa880b60ea3
Radoslav Georgiev <rgeorgiev583@gmail.com>
2022-11-02 01:33:53 +0200

    fish: replace the hardcoded `fzf_key_bindings` symlink with a script which creates it automatically
    The script is aware of the differences in the path to the keybindings
    file between the `fzf` packages in different distros.  Invoke the script
    as part of the install script.

---
+++ install-fzf-key-bindings-symlink.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source /etc/os-release
+
+function install_symlink {
+    case $1 in
+    arch|debian)
+        ln -sf /usr/share/fish/vendor_functions.d/fzf_key_bindings.fish ~/.config/fish/functions/fzf_key_bindings.fish
+        exit
+        ;;
+    fedora)
+        ln -sf /usr/share/fzf/shell/key-bindings.fish ~/.config/fish/functions/fzf_key_bindings.fish
+        exit
+        ;;
+    esac
+}
+
+install_symlink "$ID"
+
+for distro in $ID_LIKE; do
+    install_symlink "$distro"
+done
+
+echo 'error: OS not supported' >&2
+exit 1
--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ rsync -a homedir/ ~/ "$@"
 while read -r file_path macro_definition; do
     m4 -D "$macro_definition" "homedir/$file_path" > "$HOME/$file_path"
 done
+
+./install-fzf-key-bindings-symlink.sh
