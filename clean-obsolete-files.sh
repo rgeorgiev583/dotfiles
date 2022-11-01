e0e1ad4aa336723d089672d2f721468ba65e8dbb
Radoslav Georgiev <rgeorgiev583@gmail.com>
2022-11-02 01:37:14 +0200

    add a script for cleanup of target files which were deleted from the repo

---
+++ clean-obsolete-files.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+function remove_target {
+    while read -r src_file; do
+        rm -f ~/${src_file#homedir/}
+    done
+}
+
+git log --diff-filter=D --summary --pretty= | awk '{ print $4 }' | grep ^homedir/ | remove_target
+git status --porcelain | grep '^\s*D ' | awk '{ print $2 }' | grep ^homedir/ | remove_target
