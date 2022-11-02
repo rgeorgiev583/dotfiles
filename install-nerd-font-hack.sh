747e8df21f2b482d770ff7bd54536a7e7a8107c4
Radoslav Georgiev <rgeorgiev583@gmail.com>
2022-11-02 22:19:07 +0200

    install: fix the installation script for the patched Nerd Hack font
    Make `unzip` overwrite files in the destination.

--- install-nerd-font-hack.sh
+++ install-nerd-font-hack.sh
@@ -4,5 +4,5 @@ set -e
 
 mkdir -p ~/.local/share/fonts
 curl -fLo /tmp/Hack.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/Hack.zip
-unzip -d ~/.local/share/fonts /tmp/Hack.zip
+unzip -od ~/.local/share/fonts /tmp/Hack.zip
 rm /tmp/Hack.zip
