18f592e455d31b8a5bf90787e977ce8c7be6af8a
Radoslav Georgiev <rgeorgiev583@gmail.com>
2022-11-06 20:13:45 +0200

    lvim: change the colorscheme to `tokyonight-day`
    Remove the `vscode` colorscheme plugin.

--- homedir/.config/lvim/config.lua
+++ homedir/.config/lvim/config.lua
@@ -11,7 +11,7 @@ an executable
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = false
-lvim.colorscheme = "vscode"
+lvim.colorscheme = "tokyonight-day"
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
@@ -319,7 +319,6 @@ null_ls.register(cmake_lint)
 --     },
 -- }
 lvim.plugins = {
-  { "Mofiqul/vscode.nvim" },
   {
     "tpope/vim-surround",
     keys = { "c", "d", "y" }
