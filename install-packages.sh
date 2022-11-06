4c38ac0102c559640238fac5d170bdd067b358d4
Radoslav Georgiev <rgeorgiev583@gmail.com>
2022-11-06 20:13:42 +0200

    install-packages: add `m4` and `tig`
    `m4` is required by the `install-dotfiles.sh` script.  I use `tig`
    extensively when dealing with Git repos.

--- install-packages.sh
+++ install-packages.sh
@@ -5,12 +5,12 @@ source /etc/os-release
 function install_packages {
     case $1 in
     arch)
-        pacman --sync --needed --noconfirm bat fd fish fisher fzf git-delta neovim psmisc python-pip ripgrep rsync sqlite tree
+        pacman --sync --needed --noconfirm bat fd fish fisher fzf git-delta m4 neovim psmisc python-pip ripgrep rsync sqlite tig tree
         exit
         ;;
     debian)
         # `fisher` and `git-delta` are not in the official repos
-        apt-get -y update && apt-get -y install bat fd-find fish fzf neovim psmisc python3-pip ripgrep rsync sqlite3 tree &&
+        apt-get -y update && apt-get -y install bat fd-find fish fzf m4 neovim psmisc python3-pip ripgrep rsync sqlite3 tig tree &&
             ln -s /usr/bin/batcat /usr/local/bin/bat &&
             ln -s /usr/lib/cargo/bin/fd /usr/local/bin/fd &&
             ln -s /usr/bin/vim.tiny /usr/local/bin/vim
@@ -18,7 +18,7 @@ function install_packages {
     ;;
     fedora)
         # `fisher` is not in the official repos
-        yum -y install bat fd-find fish fzf git-delta neovim psmisc python-pip ripgrep rsync sqlite tree
+        yum -y install bat fd-find fish fzf git-delta m4 neovim psmisc python-pip ripgrep rsync sqlite tig tree
         exit
         ;;
     esac
