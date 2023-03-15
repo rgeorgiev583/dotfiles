45aa225da9cf644495c01bfd806f1e6b5cbc902e
Radoslav Georgiev <rgeorgiev583@gmail.com>
2023-03-15 13:26:33 +0200

    fish: add abbreviation for VSCode

--- homedir/.config/fish/conf.d/abbreviations.fish
+++ homedir/.config/fish/conf.d/abbreviations.fish
@@ -10,6 +10,7 @@ abbr --add --global df df -h
 abbr --add --global less less --tabs=4 -RFX
 abbr --add --global rga rg --hidden --no-ignore
 abbr --add --global fda fd --hidden --no-ignore
+abbr --add --global c code
 
 # Linux-specific definitions
 
