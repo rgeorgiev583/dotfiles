2dd36c0cbe06fd02128efce0a78b2d7662bd45ed
Radoslav Georgiev <rgeorgiev583@gmail.com>
2023-04-12 14:03:19 +0300

    dotfiles/fish/functions/logout: remove redundant definition args

--- homedir/.config/fish/functions/logout.fish
+++ homedir/.config/fish/functions/logout.fish
@@ -1,3 +1,3 @@
-function logout --wraps='qdbus org.kde.ksmserver /KSMServer logout 0 0 1' --description 'alias logout=qdbus org.kde.ksmserver /KSMServer logout 0 0 1'
+function logout
     qdbus-qt5 org.kde.ksmserver /KSMServer logout 0 0 1 $argv
 end
