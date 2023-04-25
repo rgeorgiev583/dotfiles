580a1239bd13fddeb21ff05ac7f2c02ae41e1d88
Radoslav Georgiev <rgeorgiev583@gmail.com>
2023-04-25 14:05:07 +0300

    dotfiles/fish: add k8s abbreviations
    Remove a conflicting abbreviation for Kate.

--- homedir/.config/fish/conf.d/abbreviations.fish
+++ homedir/.config/fish/conf.d/abbreviations.fish
@@ -9,7 +9,6 @@ abbr --add --global less less --tabs=4 -RFX
 abbr --add --global rga rg --hidden --no-ignore
 abbr --add --global fda fd --hidden --no-ignore
 abbr --add --global c code
-abbr --add --global k kate
 abbr --add --global v vim
 abbr --add --global n nvim
 abbr --add --global b bat
---
+++ homedir/.config/fish/conf.d/k8s-abbreviations.fish
@@ -0,0 +1,54 @@
+if not status --is-interactive
+    exit
+end
+
+set kubectl_cmd kubectl
+
+abbr --add --global k kubectl
+abbr --add --global ka kubectl apply
+abbr --add --global kaf kubectl apply -f
+abbr --add --global kan kubectl annotate
+abbr --add --global kar kubectl api-resources
+abbr --add --global kas kubectl autoscale
+abbr --add --global kat kubectl attach
+abbr --add --global kau kubectl auth
+abbr --add --global kav kubectl api-versions
+abbr --add --global kc kubectl create
+abbr --add --global kce kubectl certificate
+abbr --add --global kcf kubectl config
+abbr --add --global kci kubectl cluster-info
+abbr --add --global kco kubectl cordon
+abbr --add --global kd kubectl delete
+abbr --add --global kdb kubectl debug
+abbr --add --global kde kubectl describe
+abbr --add --global kdi kubectl diff
+abbr --add --global kdr kubectl drain
+abbr --add --global ke kubectl exec
+abbr --add --global ked kubectl edit
+abbr --add --global kei kubectl exec -it
+abbr --add --global kev kubectl events
+abbr --add --global kex kubectl explain
+abbr --add --global kexp kubectl expose
+abbr --add --global kg kubectl get
+abbr --add --global kgd kubectl get deployments
+abbr --add --global kgp kubectl get pods
+abbr --add --global kgs kubectl get services
+abbr --add --global kk kubectl kustomize
+abbr --add --global kl kubectl logs
+abbr --add --global kla kubectl label
+abbr --add --global kp kubectl proxy
+abbr --add --global kpa kubectl patch
+abbr --add --global kpf kubectl port-forward
+abbr --add --global kpl kubectl plugin
+abbr --add --global kr kubectl run
+abbr --add --global krd kubectl rollout restart deploy
+abbr --add --global kre kubectl replace
+abbr --add --global kro kubectl rollout
+abbr --add --global krr kubectl rollout restart
+abbr --add --global ks kubectl set
+abbr --add --global ksc kubectl scale
+abbr --add --global kt kubectl top
+abbr --add --global kta kubectl taint
+abbr --add --global kuc kubectl uncordon
+abbr --add --global kv kubectl version
+abbr --add --global kw kubectl wait
